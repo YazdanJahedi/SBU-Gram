f874cf71131bae69dd886d2acd0c867918d09cb6
Yazdan <yzdn.jt@gmail.com>
2021-05-31 10:32:58 +0430

    Common project is added . Mudules are addded to Client and Server side projects

--- Client-side/src/Model/Main.java
+++ Client-side/src/Model/Main.java
@@ -67,4 +67,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
--- Server-side/src/Messages/Message.java
+++
@@ -1,6 +0,0 @@
-package Messages;
-
-import java.io.Serializable;
-
-public interface Message extends Serializable {
-}
